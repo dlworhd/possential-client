7cf8307b86c1a2af1cecaf5761b0591f13dc61aa
dlwodud821@gmail.com <dlwodud821@gmail.com>
2023-08-19 21:14:18 +0900

    #4: Axios Request Menu Items Pagination

--- vite.config.ts
+++ vite.config.ts
@@ -12,5 +12,15 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
-})
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        configure: (proxy, options) => {
+          // proxy 변수에는 'http-proxy'의 인스턴스가 전달됩니다
+        }
+    },
+  },
+}})
