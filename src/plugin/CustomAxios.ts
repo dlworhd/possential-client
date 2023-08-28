d61475d7ce7df176ff82ab1bf557455c6d867e7a
dlwodud821@gmail.com <dlwodud821@gmail.com>
2023-08-28 21:52:07 +0900

    #53 refactor: Custom Axios Refactoring

--- src/plugin/CustomAxios.ts
+++ src/plugin/CustomAxios.ts
@@ -1,18 +1,26 @@
-import router from '../router/index';
-import { mapMutations, Store } from 'vuex';
+import { Store } from "vuex";
 import axios, {
   AxiosError,
   AxiosInstance,
   InternalAxiosRequestConfig,
 } from "axios";
-import store from '@/store';
+import store from "@/store";
 
-declare module '@vue/runtime-core' {
+declare module "@vue/runtime-core" {
   interface ComponentCustomProperties {
     $store: Store<any>;
   }
 }
 
+/**
+ *
+ * Request Interceptors
+ *
+ * HTTP
+ * withCredentials: 쿠키, 인증 헤더 및 기타 인증 정보를 자동으로 포함시켜 서버로 전송
+ * Authorization: 접두사 'Bearer '를 붙인 후 JWT Access Token을 HTTP Request Header에 담아서 전송
+ *
+ */
 const instance: AxiosInstance = axios.create({
   baseURL: "http://localhost:8080",
   withCredentials: true,
@@ -22,6 +30,7 @@ function accessToken(): string | null {
   return localStorage.getItem("accessToken");
 }
 
+//Request Interceptors
 instance.interceptors.request.use(
   async (config: InternalAxiosRequestConfig) => {
     const token = accessToken();
@@ -35,40 +44,51 @@ instance.interceptors.request.use(
   }
 );
 
+/**
+ *
+ * Response Interceptors
+ *
+ * Status Code
+ * 403 FORBIDDEN: Access Token이 존재하지 않을 때
+ * 401 UNAUTHORIZED: 권한이 없는 경우(Access Token이 존재하지만 만료된 경우 또는 Authorization과 함께 공백이 가는 경우)
+ * 400 BAD_REQUEST: Refresh Token이 존재하지 않을 때
+ *
+ * EndPoint
+ * 토큰 재발급: /api/auth/reissue
+ *
+ */
 instance.interceptors.response.use(
   (response) => response,
   async (error: AxiosError) => {
-    // 403: Access Token이 존재하지 않을 때
-    // 401: Access Token이 존재하지만 만료된 경우
-    // Refresh Token이 존재하지 않을 때 400 에러
-    if (error.response && (error.response.status === 401 || error.response.status === 403)) {
-      try {
-        localStorage.removeItem("accessToken");
-        // "/api/auth/reissue" 엔드포인트로 요청을 보내 새 토큰을 발급받음
+    const errorResponse = error.response;
 
-        const reissueResponse = await instance.post("/api/auth/reissue");
-        const newAccessToken = reissueResponse.data.value;
+    if (errorResponse) {
+      if (errorResponse.status === 401 || errorResponse.status === 403) {
+        try {
+          //
+          localStorage.removeItem("accessToken");
+          const reissueResponse = await instance.post("/api/auth/reissue");
+          const newAccessToken = reissueResponse.data.value;
 
-        // 새로 발급받은 액세스 토큰을 로컬 스토리지에 저장
-        localStorage.setItem("accessToken", newAccessToken);
+          // 새로 발급받은 액세스 토큰을 로컬 스토리지에 저장
+          localStorage.setItem("accessToken", newAccessToken);
 
-        // 원래 요청을 수정하고 새 액세스 토큰을 사용하여 다시 요청 보냄
-        if (error.config) {
-          error.config.headers["Authorization"] = `Bearer ${newAccessToken}`;
-          return instance.request(error.config);
+          // 원래 요청을 수정하고 새 액세스 토큰을 사용하여 다시 요청 보냄
+          if (error.config) {
+            error.config.headers["Authorization"] = `Bearer ${newAccessToken}`;
+            return instance.request(error.config);
+          }
+          store.commit("setLogin", true);
+        } catch (reissueError) {
+          console.log("토큰 재발급 실패:", reissueError);
+          store.commit("setLogin", false);
+          return Promise.reject(error);
         }
-        store.commit('setLogin', true);
-      } catch (reissueError) {
-        console.log("토큰 재발급에 실패했습니다:", reissueError);
-        store.commit('setLogin', false);
-        return Promise.reject(error);
       }
-    } else if (error.response && error.response.status === 400) {
-      // 400 에러일 경우에는 인터셉터 로직을 적용하지 않음
+    } else {
+      console.log('Error:',error)
       return Promise.reject(error);
     }
-    // 그 외의 에러 처리 로직
-    return Promise.reject(error);
   }
 );
 export default instance;
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,60 +1,61 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import SignUpView from '@/views/SignUpView.vue';
-import LoginForm from '@/components/user/LoginForm.vue';
-import PosView from '@/views/PosView.vue';
-import OrderHistoriesView from '@/views/OrderHistoriesView.vue'
-import KakaoPaySuccess from '@/views/KakaoPaySuccess.vue'
-import KakaoPayFail from '@/views/KakaoPayFail.vue'
-import store from '@/store';
-import NotFound from '@/components/common/NotFound.vue';
+import { createRouter, createWebHistory } from "vue-router";
+import SignUpView from "@/views/SignUpView.vue";
+import LoginForm from "@/components/user/LoginForm.vue";
+import PosView from "@/views/PosView.vue";
+import OrderHistoriesView from "@/views/OrderHistoriesView.vue";
+import KakaoPaySuccess from "@/views/KakaoPaySuccess.vue";
+import KakaoPayFail from "@/views/KakaoPayFail.vue";
+import store from "@/store";
+import NotFound from "@/components/common/NotFound.vue";
 
-const router =  createRouter({
+const router = createRouter({
   history: createWebHistory(),
   routes: [
-  {
-    path: '/sign-up',
-    component: SignUpView,
-    meta: { requiresAuth: false },
-  },
-  {
-    path: '/login',
-    component: LoginForm,
-    meta: { requiresAuth: false },
-  },
-  {
-    path: '/',
-    component: PosView,
-    meta: { requiresAuth: false },
-  },
-  {
-    path: '/orders',
-    component: OrderHistoriesView,
-    meta: { requiresAuth: true },
-  },
-  {
-    path: '/pay/success',
-    component: KakaoPaySuccess,
-    meta: { requiresAuth: true },
-  },
-  {
-    path: '/pay/fail',
-    component: KakaoPayFail,
-    meta: { requiresAuth: true },
-  },
-  {
-    path: '/:catchAll(.*)',
-    component: NotFound,
-  },
-],
-})
+    {
+      path: "/",
+      component: PosView,
+      meta: { requiresAuth: false },
+    },
+    {
+      path: "/sign-up",
+      component: SignUpView,
+      meta: { requiresAuth: false },
+    },
+    {
+      path: "/login",
+      component: LoginForm,
+      meta: { requiresAuth: false },
+    },
 
+    {
+      path: "/orders",
+      component: OrderHistoriesView,
+      meta: { requiresAuth: true },
+    },
+    {
+      path: "/pay/success",
+      component: KakaoPaySuccess,
+      meta: { requiresAuth: true },
+    },
+    {
+      path: "/pay/fail",
+      component: KakaoPayFail,
+      meta: { requiresAuth: true },
+    },
+    {
+      path: "/:catchAll(.*)",
+      component: NotFound,
+    },
+  ],
+});
+
+//Router Navigation Guard
 router.beforeEach((to, from, next) => {
-  if(to.meta.requiresAuth && !store.state.isLogin){
-    next('/login');
+  if (to.meta.requiresAuth && !store.state.isLogin) {
+    next("/login");
   } else {
     next();
   }
-})
+});
 
-
-export default router
+export default router;
